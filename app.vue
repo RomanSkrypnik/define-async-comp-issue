439d4fc57337be00b011de05a167ba8fa59f319e
Roman Skrypnik <r.skrypnik@yual.io>
2024-05-21 20:55:40 +0300

    init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <component :is="comp" />
+  <button @click="toggle = !toggle">Show</button>
+</template>
+<script setup>
+const toggle = ref(false);
+
+const comp = computed(() => toggle.value ? getTestComponent('Test') : null);
+</script>
